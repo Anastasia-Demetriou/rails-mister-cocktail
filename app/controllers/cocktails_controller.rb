5f27bc9c82d3b9aa31499a90c1e2e663b4173aa1
Anastasia Demetriou <anastasiademetriou.ad@gmail.com>
2018-11-09 15:25:25 +0000

    fixed stuff

--- app/controllers/cocktails_controller.rb
+++ app/controllers/cocktails_controller.rb
@@ -1,37 +1,56 @@
 class CocktailsController < ApplicationController
+
+  before_action :find_cocktail, only: [:show, :update, :destroy]
+
   def index
     @cocktails = Cocktail.all
   end
 
-  def show
-    @cocktail = Cocktail.find(params[:id])
+  def new
+    @cocktail = Cocktail.new
   end
 
   def create
-    @cocktails = Cocktail.new(cocktails_params)
-    @cocktails.save
+    @cocktail = Cocktail.new(cocktails_params)
+    if @cocktail.save
+      redirect_to cocktail_path(@cocktail)
+    else
+      render :new
+    end
+  end
+
+  def edit
+    @cocktail = Cocktail.new
+    if @cocktail.save
+      redirect_to edit_cocktail_path(@cocktail)
+    else
+      render :edit
+    end
+  end
+
+  def show
   end
 
   def update
-    @cocktails = Cocktail.find(params[:id])
-    @cocktails.update(cocktails_params)
-    redirect_to cocktails_path(@cocktails)
-  end
-
-  def new
-    @cocktails = Cocktail.new
-    @cocktails.save
+    if @cocktail.update(cocktails_params)
+      redirect_to edit_cocktail_path(@cocktail)
+    else
+      render :edit
+    end
   end
 
   def destroy
-    @cocktails = Cocktail.find(params[:id])
     @cocktails.destroy
     redirect_to cocktails_path
   end
 
 private
 
+  def find_cocktail
+    @cocktail = Cocktail.find(params[:id])
+  end
+
   def cocktails_params
-    params require(:cocktails).permit(:name)
+    params.require(:cocktail).permit(:name)
   end
 end
--- db/seeds.rb
+++ db/seeds.rb
@@ -1,6 +1,10 @@
 Ingredient.create(name: "lemon")
 Ingredient.create(name: "ice")
 Ingredient.create(name: "mint leaves")
+Ingredient.create(name: "tequila")
+Ingredient.create(name: "whisky")
+Ingredient.create(name: "sugar syrup")
+
 
 Cocktail.create(name: "old fashion")
 Cocktail.create(name: "negroni")
